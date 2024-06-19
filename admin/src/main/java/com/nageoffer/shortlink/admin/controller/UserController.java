a5786513e842e553c3c905a78faa31848d54d723
yezhenchen <yezhenchen@tencent.com>
2024-06-19 20:18:53 +0800

    feature: 异常处理 部分用户模块

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.nageoffer.shortlink.admin.controller;
+
+import com.nageoffer.shortlink.admin.common.convention.result.Result;
+import com.nageoffer.shortlink.admin.common.convention.result.Results;
+import com.nageoffer.shortlink.admin.dto.resp.UserRespDTO;
+import com.nageoffer.shortlink.admin.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+    @GetMapping("/api/shortlink/v1/user/{username}")
+    public Result<UserRespDTO> getUserByUserName(@PathVariable("username") String username){
+        return Results.success(userService.getUserByUserName(username));
+    }
+}
